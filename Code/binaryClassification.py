0792646e11e01bd7deab18d70c2a75e993a4342e
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-06-06 10:08:11 +0530

    Updated: made some minor changes in binary classification code

--- Code/binaryClassification.py
+++ Code/binaryClassification.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout # type: ignore
+from tensorflow.keras.optimizers import Adam # type: ignore
 
 # Set image data format
 tf.keras.backend.set_image_data_format('channels_last')
